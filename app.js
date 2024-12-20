9f6b3e90ffe5fd77d18dbbaeadb660c93140dd16
Mizakson <maxwellizakson@gmail.com>
2024-12-20 13:37:59 -0500

    Copy exact lesson files

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require("express")
-const app = express()
-const path = require("path")
-const usersRouter = require("./routes/usersRouter")
+// app.js
+const express = require("express");
+const app = express();
+const usersRouter = require("./routes/usersRouter");
 
-app.set("view engine", "ejs")
-app.use(express.urlencoded({ extended: true }))
-app.use("/", usersRouter)
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", usersRouter);
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,12 +1,10 @@
-const { Router } = require("express")
-const usersController = require("../controllers/usersController")
-const usersRouter = Router()
+// routes/usersRouter.js
+const { Router } = require("express");
+const usersController = require("../controllers/usersController");
+const usersRouter = Router();
 
-usersRouter.get("/", usersController.usersListGet)
-usersRouter.get("/create", usersController.usersCreateGet)
-usersRouter.post("/create", usersController.usersCreatePost)
-usersRouter.get("/:id/update", usersController.usersUpdateGet)
-usersRouter.post("/:id/update", usersController.usersUpdatePost)
-usersRouter.post("/:id/delete", usersController.usersDeletePost)
+usersRouter.get("/", usersController.usersListGet);
+usersRouter.get("/create", usersController.usersCreateGet);
+usersRouter.post("/create", usersController.usersCreatePost);
 
-module.exports = usersRouter
+module.exports = usersRouter;
